b0e1fa027a8be790d415f6ead62057b75f19f86a
Haendel Ribeiro <haendelribeiro@gmail.com>
2025-11-13 19:28:40 -0300

    Reformat calcularMontante function and prompts

--- FRONT_END_2_ATIVIDADE_5.js
+++ FRONT_END_2_ATIVIDADE_5.js
@@ -2,9 +2,9 @@
 
 function calcularMontante(C, i, t) {
 
-&nbsp;   let montante = C \* Math.pow(1 + (i / 100), t);  // i em %
+let montante = C \* Math.pow(1 + (i / 100), t);  // i em %
 
-&nbsp;   console.log("Montante: R$ " + montante.toFixed(2));
+  console.log("Montante: R$ " + montante.toFixed(2));
 
 }
 
@@ -15,6 +15,7 @@ let i = parseFloat(prompt("Digite a taxa de juros mensal (%):"));
 let t = parseInt(prompt("Digite o tempo em meses (t):"));
 
 calcularMontante(C, i, t);
+
 
 
 
